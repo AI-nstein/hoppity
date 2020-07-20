d3338a1da151dcad48e68029dbe511b08abc0351
Elizabeth Dinella <elizabeth.a.dinella@gmail.com>
2020-07-20 13:28:38 -0400

    Added training script

---
+++ gtrans/training/run_main.sh
@@ -0,0 +1,15 @@
+cooked_root=[INPUT_DIR]
+data_name=[NAME]
+
+save_dir=[OUTPUT_DIR]
+
+python main_gtrans.py \
+	-data_root $cooked_root \
+	-data_name $data_name \
+	-save_dir $save_dir \
+	-gnn_type 's2v_multi' \
+	-max_lv 4 \
+	-max_modify_steps 1 \
+	-resampling True \
+	-comp_methd "mlp" \
+	$@
